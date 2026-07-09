dd11a542156120c50c8358df9afcad259ed4a4d4
Wesley Wu <143961832+lilwon17@users.noreply.github.com>
2026-07-09 10:52:07 -0700

    Added member functions to Card class header file
    Added constructors and member functions to Card class.

--- Card.hpp
+++ Card.hpp
@@ -11,8 +11,55 @@ private:
 
 public:
 
+    // Constructors
 
+    // Default
+    // Postcondition:
+    // Create a variable of class Card with rank(1) and suit(0)
+    Card();
 
+    // Specific
+    // Postcondition:
+    // Create a variable of class Card with rank(r) and suit(s)
+    Card(int r, int s);
+
+    // Member Functions
+
+    // isFaceCard() - Check Face Card
+    // Precondition:
+    // Card rank must be valid; Card must be a valid card
+    // Postcondition:
+    // Return true if card has rank value greater or equal to 11 and false if otherwise
+    bool isFaceCard();
+
+    // isValid() - Check if card is valid
+    // Precondition:
+    // Card components must be numerical values
+    // Postcondition:
+    // If the value for rank or suit is invalid return false and error message
+    // else, return true and a message saying the card is valid
+    bool isValid();
+
+    /* Mapping
+
+    * 0 = Clubs
+    * 1 = Diamonds
+    * 2 = Hearts
+    * 3 = Spades
+
+    * 1 = Ace
+    ...
+    * 11 = Jack
+    * 12 = Queen
+    * 13 = King
+    
+    */
+
+    // Overload Insertion Operator
+    friend ostream& operator<<(ostream& os, const Card& c);
+
+    // Overload Extraction Operator
+    friend ostream& operator<<(ostream& is, Card& c);
 
 };
 
