50165ad74e0362146199003b3f868ce971587d97
Wesley Wu <143961832+lilwon17@users.noreply.github.com>
2026-08-06 15:23:21 -0700

    Change displayHiddenHand to const
    Updated displayHiddenHand method to be a const member function.

--- Dealer.hpp
+++ Dealer.hpp
@@ -70,7 +70,7 @@ public:
     // Then the dealer draws until its hand value is 17 or higher or until the dealer busts
 
     // Displaying the dealer's hand with a hidden card
-    void displayHiddenHand(const Hand& hand);
+    void displayHiddenHand() const;
     // Precondition:
     // Dealer must have two cards in their hand
     // Postcondition:
